653aa0cc5fd8be648d246f96a4ade71f9cea33da
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-03-19 23:47:14 +0300

    [PayrollSystemToPackages] Init submit
    -added Date package
    -added test for Date packege

---
+++ Object-oriented_programming/PayrollSystemToPackages/include/Date/DateTest.h
@@ -0,0 +1,15 @@
+#ifndef __DATETEST_H__
+#define __DATETEST_H__
+
+namespace DateTest
+{
+	void TestAmountOfDaysMonth();
+	void TestAmountOfDaysYear();
+	void TestOrderOfDate();
+	void TestAddDays();
+	void TestGetDate();
+	void TestSubtractionDays();
+	void TestGetDayOfWeek();
+}
+
+#endif // !__DATETEST_H__
