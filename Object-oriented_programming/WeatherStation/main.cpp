8c7c12aaae02d7feb241a2d2997f2f9affe7994e
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-04-30 09:14:21 +0300

    [WeatherStation] First realisation main`s program

--- Object-oriented_programming/WeatherStation/main.cpp
+++ Object-oriented_programming/WeatherStation/main.cpp
@@ -1,10 +1,30 @@
 
+#include "TemperatureSensor.h"
+#include "BarometricPressureSensor.h"
+
 #include "TestTemperatureSensor.h"
 #include "TestBarometricPressureSensor.h"
 #include "MonitoringScreenImplementation.h"
 #include "BarometricPressureTrendSensor.h"
+#include "AlarmClock.h"
 
 int main(int argc, char* argv[])
 {
+	AlarmClock* ac = new AlarmClock();
+	TemperatureSensor* ts = new TemperatureSensor(ac, new TestTemperatureSensor(-10, 10));
+	BarometricPressureSensor* bps = new BarometricPressureSensor(ac, new TestBarometricPressureSensor(365, 380));
+	BarometricPressureTrendSensor* bpts = new BarometricPressureTrendSensor(bps);
+
+	MonitoringScreenImplementation msi(ts, bps, bpts);
+	int numberCall = 10;
+	for (int i = 0; i < numberCall; i++)
+	{
+		ac->wakeupAll();
+	}
+
+	delete bpts;
+	delete bps;
+	delete ts;
+	delete ac;
 	return 0;
 }
