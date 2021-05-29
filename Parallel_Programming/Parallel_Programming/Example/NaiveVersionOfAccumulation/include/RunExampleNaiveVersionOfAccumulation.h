aaceeb5db37b2fac72fa2a43f2a83d4dcb89b400
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-05-29 08:25:21 +0300

    [Parallel_Programming] Added NaiveVersionOfAccumulation

---
+++ Parallel_Programming/Parallel_Programming/Example/NaiveVersionOfAccumulation/include/RunExampleNaiveVersionOfAccumulation.h
@@ -0,0 +1,19 @@
+#ifndef __RUNEXAMPLENAIVEVERSIONOFACCUMULATION_H__
+#define __RUNEXAMPLENAIVEVERSIONOFACCUMULATION_H__
+
+#include "RunCommandOfExample.h"
+
+/**
+* Implementation for running examples NaiveVersionOfAccumulation
+* @see <numeric> std::accumulate
+*/
+class RunExampleNaiveVersionOfAccumulation : public RunCommandOfExample
+{
+public:
+	/**
+	* Example run command
+	*/
+	virtual void excute() override;
+};
+
+#endif // !__RUNEXAMPLENAIVEVERSIONACCUMULATION_H__
