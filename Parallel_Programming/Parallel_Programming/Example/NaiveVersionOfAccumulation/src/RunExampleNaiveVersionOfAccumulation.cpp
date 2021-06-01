eeb40875c93b0d81df409616e928f785e40b5113
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-06-01 12:09:07 +0300

    [Parallel_Programming] Added comment to example NaiveVersionOfAccumulation

--- Parallel_Programming/Parallel_Programming/Example/NaiveVersionOfAccumulation/src/RunExampleNaiveVersionOfAccumulation.cpp
+++ Parallel_Programming/Parallel_Programming/Example/NaiveVersionOfAccumulation/src/RunExampleNaiveVersionOfAccumulation.cpp
@@ -10,54 +10,65 @@
 
 RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new RunExampleNaiveVersionOfAccumulation();
 
-template<typename Iterator, typename T>
-struct AccumulateBlock
-{
-	void operator()(Iterator first, Iterator last, T& result)
+namespace {
+	template<typename Iterator, typename T>
+	struct AccumulateBlock
 	{
-		result = std::accumulate(first, last, result);
+		void operator()(Iterator first, Iterator last, T& result)
+		{
+			result = std::accumulate(first, last, result);
+
+#ifdef DEBUG_BUILD
+			std::cout << result << std::endl;
+#endif
+		}
+	};
+
+	template<typename Iterator, typename T>
+	T parallelAccumulate(Iterator first, Iterator last, T init)
+	{
+		size_t const length = std::distance(first, last);
+		if (!length)
+		{
+			return init;
+		}
+
+		// This number is amount elements will be processed at one thread
+		size_t const minPerThread = 25;
+
+		// if we will use formula:
+		// maxThreads = length / minPerThred,
+		// we can get zero value of maxThreads
+		size_t const maxThreads = (length + minPerThread - 1) / minPerThread;
+
+		size_t const hardWareThreads = std::thread::hardware_concurrency();
+
+		// Amount Thread will be used
+		size_t const numThreads = std::min(hardWareThreads != 0 ? hardWareThreads : 2, maxThreads);
+
+#ifdef DEBUG_BUILD
+		std::cout << "numThreads:\t" << numThreads << std::endl;
+#endif
 		
-		std::cout << result << std::endl;
+
+		size_t const blockSize = length / numThreads;
+
+		std::vector<T> results(numThreads);
+		std::vector<std::thread> threads(numThreads - 1);
+		Iterator blockStart = first;
+		for (size_t i = 0; i < (numThreads - 1); i++)
+		{
+			Iterator blockEnd = blockStart;
+			std::advance(blockEnd, blockSize);
+			threads[i] = std::thread(AccumulateBlock<Iterator, T>(), blockStart, blockEnd, std::ref(results[i]));
+			blockStart = blockEnd;
+		}
+		AccumulateBlock<Iterator, T>()(blockStart, last, results[numThreads - 1]);
+
+		std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+
+		return std::accumulate(results.begin(), results.end(), init);
 	}
-};
-
-template<typename Iterator, typename T>
-T parallelAccumulate(Iterator first, Iterator last, T init)
-{
-	size_t const length = std::distance(first, last);
-	if (!length)
-	{
-		return init;
-	}
-
-	// This number is amount elements will be processed at one thread
-	size_t const minPerThread = 25;
-	size_t const maxThreads = (length + minPerThread - 1) / minPerThread;
-
-	size_t const hardWareThreads = std::thread::hardware_concurrency();
-
-	// Amount Thread will be used
-	size_t const numThreads = std::min(hardWareThreads != 0 ? hardWareThreads : 2, maxThreads);
-
-	std::cout << "numThreads:\t" << numThreads << std::endl;
-
-	size_t const blockSize = length / numThreads;
-
-	std::vector<T> results(numThreads);
-	std::vector<std::thread> threads(numThreads - 1);
-	Iterator blockStart = first;
-	for (size_t i = 0; i < (numThreads - 1); i++)
-	{
-		Iterator blockEnd = blockStart;
-		std::advance(blockEnd, blockSize);
-		threads[i] = std::thread(AccumulateBlock<Iterator, T>(), blockStart, blockEnd, std::ref(results[i]));
-		blockStart = blockEnd;
-	}
-	AccumulateBlock<Iterator,T>()(blockStart, last, results[numThreads - 1]);
-	
-	std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
-
-	return std::accumulate(results.begin(), results.end(), init);
 }
 
 void RunExampleNaiveVersionOfAccumulation::excute()
