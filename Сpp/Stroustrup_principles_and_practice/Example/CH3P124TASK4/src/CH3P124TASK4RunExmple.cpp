67d276093f2bb3848ce7f3f0c875b6abd7647259
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-10-16 12:03:41 +0300

    Added example CH3P124TASK4
    CH3P124TASK4	/ Add new variable friend_sex and the string "If you see friend_name, please ask him/her to call me."

--- Сpp/Stroustrup_principles_and_practice/Example/CH3P124TASK4/src/CH3P124TASK4RunExmple.cpp
+++ Сpp/Stroustrup_principles_and_practice/Example/CH3P124TASK4/src/CH3P124TASK4RunExmple.cpp
@@ -1,18 +1,18 @@
 /*
 	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
-	date generate                                : 10/16/2021 11:20:54
+	date generate                                : 10/16/2021 11:44:37
 	author                                       : Andrei-KS
 */
 
-#include "CH3P124TASK3RunExmple.h"
+#include "CH3P124TASK4RunExmple.h"
 #include "std_lib_facilities.h"
 
-RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH3P124TASK3RunExmple();
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH3P124TASK4RunExmple();
 
 namespace {
 }
 
-int CH3P124TASK3RunExmple::excute()
+int CH3P124TASK4RunExmple::excute()
 {
 	cout << "Enter the recipient's name (and press 'Enter):\n";
 	string first_name;
@@ -20,8 +20,19 @@ int CH3P124TASK3RunExmple::excute()
 	cout << "Enter another friend's name (and press 'Enter):\n";
 	string friend_name;
 	cin >> friend_name;
+	cout << "Enter m/f corresponding of friends sex (m - male, f - female):\n";
+	char friend_sex = ' ';
+	cin >> friend_sex;
 	cout << "Dear " << first_name << ",\n"
 		<< "How are you?\nI'm fine. I miss you.\n"
 		<< "How long have you met " << friend_name << "?\n";
+	if (friend_sex == 'm')
+	{
+		cout << "If you see " << friend_name  << ", please ask him to call me.\n";
+	}
+	else if (friend_sex == 'f')
+	{
+		cout << "If you see " << friend_name << ", please ask her to call me.\n";
+	}
 	return 0;
 }
