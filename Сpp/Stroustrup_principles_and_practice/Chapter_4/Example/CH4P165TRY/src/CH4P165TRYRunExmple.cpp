d70bba25c8cd3bd16d951a16ba2c730ddf889b28
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-10-17 18:23:37 +0300

    Added CH4P165TRY example
    CH4P165TRY			/ Implementation a program to muffle unwanted words

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_4/Example/CH4P165TRY/src/CH4P165TRYRunExmple.cpp
@@ -0,0 +1,43 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 10/17/2021 18:13:19
+	author                                       : Andrei-KS
+*/
+
+#include "CH4P165TRYRunExmple.h"
+#include "std_lib_facilities.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH4P165TRYRunExmple();
+
+namespace {
+}
+
+int CH4P165TRYRunExmple::excute()
+{
+	vector<string> words;
+	vector<string> blockList = { "Broccoli" };
+	for (string temp; cin >> temp;)
+	{
+		words.push_back(temp);
+	}
+	cout << "Words number: " << words.size() << "\n";
+
+	sort(words);
+	for (int i = 0; i < words.size(); ++i)
+	{
+		if (i == 0 || words[i - 1] != words[i])
+		{
+			bool isDisply = true;
+			for (const string blockWord : blockList)
+			{
+				if (words[i] == blockWord)
+				{
+					isDisply = false;
+					break;
+				}
+			}
+			if(isDisply) cout << words[i] << "\n";
+		}
+	}
+	return 0;
+}
