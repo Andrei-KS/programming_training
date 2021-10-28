2dbe6cf258271866e477cdac63760acdb1f87379
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-10-29 00:03:03 +0300

    Added CH5P199TRY example
    CH5P199TRY			/ Set valid min and max value

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_5/Example/CH5P199TRY/include/CH5P199TRYRunExmple.h
@@ -0,0 +1,28 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 10/28/2021 23:55:28
+	author                                       : Andrei-KS
+*/
+#ifndef __CH5P199TRY__
+#define __CH5P199TRY__
+
+#include "RunCommandOfExample.h"
+
+/**
+* 
+* @author Andrei-KS
+*/
+class CH5P199TRYRunExmple : public RunCommandOfExample
+{
+public:
+	/**
+	 * The main function launch to run a example.
+	 * This function can use command line parameters
+	 * via self variables itsArgC, itsArgV
+	 *
+	 * @see RunCommandOfExample
+	 */
+	virtual int excute() override;
+};
+
+#endif // !__CH5P199TRY__
---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_5/Example/CH5P199TRY/src/CH5P199TRYRunExmple.cpp
@@ -0,0 +1,39 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 10/28/2021 23:55:28
+	author                                       : Andrei-KS
+*/
+
+#include "CH5P199TRYRunExmple.h"
+#include "std_lib_facilities.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH5P199TRYRunExmple();
+
+namespace {
+	void funtion_sum_high_low(const vector<double> temps)
+	{
+		constexpr double min_temp = -459.67;	// [F], this temperature value equal 0 K
+		constexpr double max_temp = 1.40e+32;	// [F], this temperature value equal the Planck temperature
+		double sum = 0;
+		double high_temp = min_temp;
+		double low_temp = max_temp;
+		for (double x : temps)
+		{
+			if (x > high_temp) high_temp = x;
+			if (x < low_temp) low_temp = x;
+			sum += x;
+		}
+
+		cout << "Max\t: " << high_temp << "\n";
+		cout << "Min\t: " << low_temp << '\n';
+		cout << "Sum\t: " << sum << "\n";
+	}
+}
+
+int CH5P199TRYRunExmple::excute()
+{
+	funtion_sum_high_low({ 1.0, 2.0, 3.0, });
+	funtion_sum_high_low({ -1.0, -2.0, -3.0, });
+	funtion_sum_high_low({ 1.1, 1.1, 1.1, });
+	return 0;
+}
