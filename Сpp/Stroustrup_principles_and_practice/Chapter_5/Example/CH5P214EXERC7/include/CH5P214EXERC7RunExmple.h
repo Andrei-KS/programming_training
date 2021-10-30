b746db379de06000ad220c0d8cca547fcec0d576
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-10-31 00:15:04 +0300

    Added CH5P214EXERC7 example
    CH5P214EXERC7		/ Quadratic equation

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_5/Example/CH5P214EXERC7/include/CH5P214EXERC7RunExmple.h
@@ -0,0 +1,28 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 10/30/2021 22:18:01
+	author                                       : Andrei-KS
+*/
+#ifndef __CH5P214EXERC7__
+#define __CH5P214EXERC7__
+
+#include "RunCommandOfExample.h"
+
+/**
+* 
+* @author Andrei-KS
+*/
+class CH5P214EXERC7RunExmple : public RunCommandOfExample
+{
+public:
+	/**
+	 * The main function launch to run a example.
+	 * This function can use command line parameters
+	 * via self variables itsArgC, itsArgV
+	 *
+	 * @see RunCommandOfExample
+	 */
+	virtual int excute() override;
+};
+
+#endif // !__CH5P214EXERC7__
---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_5/Example/CH5P214EXERC7/src/CH5P214EXERC7RunExmple.cpp
@@ -0,0 +1,41 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 10/30/2021 22:18:01
+	author                                       : Andrei-KS
+*/
+
+#include "CH5P214EXERC7RunExmple.h"
+#include "std_lib_facilities.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH5P214EXERC7RunExmple();
+
+namespace {
+	void get_quadratic_roots(double a, double b, double c)
+	{
+		double discriminant = b * b - 4.0 * a * c;
+		if (discriminant < 0)
+		{
+			cout << "Roots are not exist.\n";
+		}
+		else if(discriminant == 0)
+		{
+			double res = -b / 2.0 / a;
+			res = res == -0 ? -res : res;
+			cout << "Root is equal " << res << "\n";
+		}
+		else
+		{
+			double sqrtDiscriminant = sqrt(discriminant);
+			cout << "First root is equal " << (-b + sqrtDiscriminant) / 2.0 / a << "\n";
+			cout << "Second root is equal " << (-b - sqrtDiscriminant) / 2.0 / a << "\n";
+		}
+		cout << "\n";
+	}
+}
+
+int CH5P214EXERC7RunExmple::excute()
+{
+	get_quadratic_roots(1, 0, 0);
+	get_quadratic_roots(1, 0, -1);
+	return 0;
+}
