29598e53880d97b7b758e6c8137ef3c30699692c
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-10-30 21:25:49 +0300

    Added CH5P214EXERC6 example
    CH5P214EXERC6		/ Celsius to fahrenheit and fahrenheit to celsius

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_5/Example/CH5P214EXERC6/src/CH5P214EXERC6RunExmple.cpp
@@ -0,0 +1,68 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 10/30/2021 21:16:37
+	author                                       : Andrei-KS
+*/
+
+#include "CH5P214EXERC6RunExmple.h"
+#include "std_lib_facilities.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH5P214EXERC6RunExmple();
+
+namespace {
+	constexpr double ABSOLUT_TEMPERATURE_ZERO_IN_C = -273.15;	// [C]
+	constexpr double ABSOLUT_TEMPERATURE_ZERO_IN_K = 0.0;			// [K]
+	constexpr double ABSOLUT_TEMPERATURE_ZERO_IN_F = -459.67;	// [F]
+	double ctok(double c)
+	{
+		if (c < ABSOLUT_TEMPERATURE_ZERO_IN_C)
+		{
+			ostringstream os;
+			os << "the temperature can not be less than " << ABSOLUT_TEMPERATURE_ZERO_IN_C << "\n";
+			error(os.str());
+		}
+		double k = c + ABSOLUT_TEMPERATURE_ZERO_IN_C;
+		return k;
+	}
+	double ktoc(double k)
+	{
+		if (k < ABSOLUT_TEMPERATURE_ZERO_IN_K)
+		{
+			ostringstream os;
+			os << "the temperature can not be less than " << ABSOLUT_TEMPERATURE_ZERO_IN_K << "\n";
+			error(os.str());
+		}
+		return k + ABSOLUT_TEMPERATURE_ZERO_IN_C;
+	}
+	double ftoc(double f)
+	{
+		if (f < ABSOLUT_TEMPERATURE_ZERO_IN_F)
+		{
+			ostringstream os;
+			os << "the temperature can not be less than " << ABSOLUT_TEMPERATURE_ZERO_IN_F << "\n";
+			error(os.str());
+		}
+		return (f - 32.0) * 5.0 / 9.0;
+	}
+	double ctof(double c)
+	{
+		if (c < ABSOLUT_TEMPERATURE_ZERO_IN_C)
+		{
+			ostringstream os;
+			os << "the temperature can not be less than " << ABSOLUT_TEMPERATURE_ZERO_IN_C << "\n";
+			error(os.str());
+		}
+		return c *9.0/5.0 + 32;
+	}
+}
+
+int CH5P214EXERC6RunExmple::excute()
+{
+	cout << "Entry temperature in [F]: ";
+	double temp_f = 0;
+	cin >> temp_f;
+	double temp_c = ftoc(temp_f);
+	cout << temp_c << '\n';
+	cout << ctof(temp_c) << "\n";
+	return 0;
+}
