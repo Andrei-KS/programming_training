8a7abd653d9ae817d267e5d55be33024f218f894
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2021-12-22 01:10:09 +0300

    [Stroustrup_principles_and_practice] Added CH8P300EXERC1 example
    CH8P300EXERC1				/ Modify the calculator program to make the input stream an explicit parameter, rather than simply using cin.

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_8/Example/CH8P300EXERC1/include/CH8P300EXERC1RunExmple.h
@@ -0,0 +1,28 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 12/21/2021 10:40:18
+	author                                       : Andrei-KS
+*/
+#ifndef __CH8P300EXERC1__
+#define __CH8P300EXERC1__
+
+#include "RunCommandOfExample.h"
+
+/**
+* 
+* @author Andrei-KS
+*/
+class CH8P300EXERC1RunExmple : public RunCommandOfExample
+{
+public:
+	/**
+	 * The main function launch to run a example.
+	 * This function can use command line parameters
+	 * via self variables itsArgC, itsArgV
+	 *
+	 * @see RunCommandOfExample
+	 */
+	virtual int excute() override;
+};
+
+#endif // !__CH8P300EXERC1__
