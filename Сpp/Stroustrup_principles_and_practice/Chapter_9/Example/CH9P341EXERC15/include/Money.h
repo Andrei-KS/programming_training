97b838b406ee0c842289633a24b7458c1beae2bc
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2022-02-04 01:02:06 +0300

    [Stroustrup_principles_and_practice] Added CH9P341EXERC15 example
    CH9P341EXERC15				/ Refine the Money class by adding a currency.

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_9/Example/CH9P341EXERC15/include/Money.h
@@ -0,0 +1,69 @@
+#ifndef __MONEY_H__
+#define __MONEY_H__
+
+namespace MyLib
+{
+	enum class Currency
+	{
+		USD = 1,
+		EUR,
+		CNY,
+		RUB
+	};
+
+	/*
+	*/
+	class Money
+	{
+	private:
+		/*the amount of monetary units of the minimum denomination of the currency */
+		long int itsAmount;
+
+		/**/
+		Currency itsCurrency;
+
+	public:
+		/* to throw as exception */
+		class Invalid {};
+
+		/* 
+		*check for valid date and initialize
+		*@param amount amountCents - the number of monetary units of the minimum denomination of the currency 
+		*/
+		explicit Money(const long int amountCents,const Currency);
+
+		/* default constructor */
+		explicit Money();
+
+		// the default copy operations are fine
+
+		// nonmodifying operations:
+		/**/
+		long amount() const { return itsAmount; }
+		/**/
+		Currency currency() const { return itsCurrency; }
+	};
+
+	/*
+	* function use the 4/5 rounding rule for rounding double to hundredths.
+	* .5 of a hundredths rounds up, anything less than .5 rounds down
+	* @param value - will be rounded 
+	* @return rounding result 
+	*/
+	long int rounding_to_hundredths(double value);
+	
+	/**/
+	Money currency_exchange(const Money& sources, const Currency type);
+	
+	bool operator==(const Money& left, const Money& right);
+	bool operator!=(const Money& left, const Money& right);
+
+	Money operator+(const Money& left, const Money& right);
+	Money operator-(const Money& left, const Money& right);
+
+	Money operator*(const Money& left, const double right);
+	Money operator*(const double left, const Money& right);
+	Money operator/(const Money& left, double right);
+}
+
+#endif // !__MONEY_H__
---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_9/Example/CH9P341EXERC15/src/CH9P341EXERC15RunExmple.cpp
@@ -0,0 +1,66 @@
+/*
+	This file is created from a mocks that is in : C:/GitHub/programming_training/Сpp/Stroustrup_principles_and_practice/include
+	date generate                                : 02/02/2022 09:05:59
+	author                                       : Andrei-KS
+*/
+
+#include "CH9P341EXERC15RunExmple.h"
+#include "std_lib_facilities.h"
+#include "Money.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH9P341EXERC15RunExmple();
+
+namespace {
+	void test_sum_money_and_money(const MyLib::Money& left, const MyLib::Money& right, const MyLib::Money correctValue)
+	{
+		MyLib::Money result = left + right;
+		if (result != correctValue)
+		{
+			error("test_sum_money_and_money():: Fail");
+		}
+	}
+
+	void test_subtraction_money_and_money(const MyLib::Money& left, const MyLib::Money& right, const MyLib::Money correctValue)
+	{
+		MyLib::Money result = left - right;
+		if (result != correctValue)
+		{
+			error("test_sum_money_and_money():: Fail");
+		}
+	}
+}
+
+int CH9P341EXERC15RunExmple::excute()
+{
+	try
+	{
+		test_sum_money_and_money(MyLib::Money(1,MyLib::Currency::USD), MyLib::Money(1, MyLib::Currency::USD), MyLib::Money(2, MyLib::Currency::USD));
+		test_sum_money_and_money(MyLib::Money(10,MyLib::Currency::USD), MyLib::Money(10, MyLib::Currency::USD), MyLib::Money(20, MyLib::Currency::USD));
+		test_sum_money_and_money(MyLib::Money(100,MyLib::Currency::USD), MyLib::Money(100, MyLib::Currency::USD), MyLib::Money(200, MyLib::Currency::USD));
+		test_sum_money_and_money(MyLib::Money(1,MyLib::Currency::USD), MyLib::Money(1, MyLib::Currency::EUR), MyLib::Money(2, MyLib::Currency::USD));
+		test_sum_money_and_money(MyLib::Money(10,MyLib::Currency::USD), MyLib::Money(10, MyLib::Currency::EUR), MyLib::Money(21, MyLib::Currency::USD));
+		test_sum_money_and_money(MyLib::Money(100,MyLib::Currency::USD), MyLib::Money(100, MyLib::Currency::EUR), MyLib::Money(213, MyLib::Currency::USD));
+		test_sum_money_and_money(MyLib::Money(1,MyLib::Currency::EUR), MyLib::Money(1, MyLib::Currency::CNY), MyLib::Money(1, MyLib::Currency::EUR));
+		test_sum_money_and_money(MyLib::Money(10,MyLib::Currency::EUR), MyLib::Money(10, MyLib::Currency::CNY), MyLib::Money(11, MyLib::Currency::EUR));
+		test_sum_money_and_money(MyLib::Money(100,MyLib::Currency::EUR), MyLib::Money(100, MyLib::Currency::CNY), MyLib::Money(114, MyLib::Currency::EUR));
+		test_sum_money_and_money(MyLib::Money(0,MyLib::Currency::RUB), MyLib::Money(1, MyLib::Currency::EUR), MyLib::Money(86, MyLib::Currency::RUB));
+	}
+	catch (const std::exception& e)
+	{
+		cout << "Test 1: " << e.what() << "\n";
+	}
+
+	try
+	{
+		test_subtraction_money_and_money(MyLib::Money(1, MyLib::Currency::USD), MyLib::Money(1, MyLib::Currency::USD), MyLib::Money(0, MyLib::Currency::USD));
+		test_subtraction_money_and_money(MyLib::Money(1, MyLib::Currency::USD), MyLib::Money(1, MyLib::Currency::EUR), MyLib::Money(0, MyLib::Currency::USD));
+		test_subtraction_money_and_money(MyLib::Money(10, MyLib::Currency::USD), MyLib::Money(10, MyLib::Currency::EUR), MyLib::Money(-1, MyLib::Currency::USD));
+
+	}
+	catch (const std::exception& e)
+	{
+		cout << "Test 2: " << e.what() << "\n";
+	}
+
+	return 0;
+}
