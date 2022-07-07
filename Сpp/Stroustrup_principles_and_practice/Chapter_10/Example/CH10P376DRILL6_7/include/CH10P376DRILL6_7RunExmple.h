78916e6fa5e25e8591e2db103b55cf11796da4b8
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2022-07-07 17:47:53 +0300

    [Stroustrup_principles_and_practice] Added CH10P376DRILL6_7 example
    CH10P376DRILL6_7   / Print the data elements from both vectors. Compare the two vectors and print Something's wrong! if the number of elements or the values of elements differ

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_10/Example/CH10P376DRILL6_7/include/CH10P376DRILL6_7RunExmple.h
@@ -0,0 +1,28 @@
+/*
+	This file is generated from a layout that is in : {PROJECT_SOURCE_DIR}/include
+	date generate												: 07/07/2022 17:31:55
+	author														: Andrei-KS
+*/
+#ifndef __CH10P376DRILL6_7__
+#define __CH10P376DRILL6_7__
+
+#include "RunCommandOfExample.h"
+
+/**
+* 
+* @author Andrei-KS
+*/
+class CH10P376DRILL6_7RunExmple : public RunCommandOfExample
+{
+public:
+	/**
+	 * The main function launch to run a example.
+	 * This function can use command line parameters
+	 * via self variables itsArgC, itsArgV
+	 *
+	 * @see RunCommandOfExample
+	 */
+	virtual int excute() override;
+};
+
+#endif // !__CH10P376DRILL6_7__
