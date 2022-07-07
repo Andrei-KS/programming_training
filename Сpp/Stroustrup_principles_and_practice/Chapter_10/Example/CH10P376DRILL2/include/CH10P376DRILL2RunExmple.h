1713a4abb411b8865e9376d2f96f83d8d24f3278
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2022-07-07 16:24:17 +0300

    On master: [Stroustrup_principles_and_practice] Added CH10P376DRILL2 example
    CH10P376DRILL2    / Prompt the user to input seven (x,y) pairs. As the data is entered, store it in a vector of Points called original_points

---
+++ Сpp/Stroustrup_principles_and_practice/Chapter_10/Example/CH10P376DRILL2/include/CH10P376DRILL2RunExmple.h
@@ -0,0 +1,28 @@
+/*
+	This file is generated from a layout that is in : {PROJECT_SOURCE_DIR}/include
+	date generate												: 07/07/2022 15:13:12
+	author														: Andrei-KS
+*/
+#ifndef __CH10P376DRILL2__
+#define __CH10P376DRILL2__
+
+#include "RunCommandOfExample.h"
+
+/**
+* 
+* @author Andrei-KS
+*/
+class CH10P376DRILL2RunExmple : public RunCommandOfExample
+{
+public:
+	/**
+	 * The main function launch to run a example.
+	 * This function can use command line parameters
+	 * via self variables itsArgC, itsArgV
+	 *
+	 * @see RunCommandOfExample
+	 */
+	virtual int excute() override;
+};
+
+#endif // !__CH10P376DRILL2__
