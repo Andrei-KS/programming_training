49b888e2965f73e322a1e9798b222ad9f204af6b
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2022-08-16 17:27:13 +0300

    [Stroustrup_principles_and_practice] Added CH12P439EXERC13_FLTK example
    CH12P439EXERC13_FLTK      / Find a way to add color to the lines from the previous exercise. Make some lines one color and other lines another color or other colors.

---
+++ Cpp/Stroustrup_principles_and_practice/Chapter_12/Example/CH12P439EXERC13_FLTK/include/CH12P439EXERC13_FLTKRunExmple.h
@@ -0,0 +1,30 @@
+/*
+	This file is generated from a layout that is in : {PROJECT_SOURCE_DIR}/include
+	date generate                                   : 08/16/2022 17:18:29
+	author                                          : Andrei-KS
+*/
+#ifndef __CH12P439EXERC13_FLTK__
+#define __CH12P439EXERC13_FLTK__
+
+#include "RunCommandOfExample.h"
+
+/*
+* Find a way to add color to the lines from the previous exercise. Make
+* some lines one color and other lines another color or other colors.
+*
+* @author Andrei-KS
+*/
+class CH12P439EXERC13_FLTKRunExmple : public RunCommandOfExample
+{
+public:
+  /*
+  * The main function launch to run a example.
+	* This function can use command line parameters
+	* via self variables itsArgC, itsArgV
+	*
+	* @see RunCommandOfExample
+	*/
+  virtual int excute() override;
+};
+
+#endif // !__CH12P439EXERC13_FLTK__
