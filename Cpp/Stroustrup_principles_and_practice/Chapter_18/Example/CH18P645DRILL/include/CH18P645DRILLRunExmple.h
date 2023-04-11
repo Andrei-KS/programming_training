bd244eacf9b5ae264aba020624ae8c58d8a8f966
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2023-04-11 08:56:53 +0200

    [Stroustrup_principles_and_practice] Added CH18P645DRILL example
    # CH18P645DRILL             / We really mean it: do run this example and make sure you understand the result. If you do, you’ll understand most of what there is to know about construction and destruction of objects.

---
+++ Cpp/Stroustrup_principles_and_practice/Chapter_18/Example/CH18P645DRILL/include/CH18P645DRILLRunExmple.h
@@ -0,0 +1,31 @@
+/*
+	This file is generated from a layout that is in : {PROJECT_SOURCE_DIR}/include
+	date generate                                   : 04/11/2023 08:46:51
+	author                                          : Andrei-KS
+*/
+#ifndef __CH18P645DRILL__
+#define __CH18P645DRILL__
+
+#include "RunCommandOfExample.h"
+
+/*
+* We really mean it: do run this example and make sure you understand the
+* result. If you do, you’ll understand most of what there is to know about
+* construction and destruction of objects.
+*
+* @author Andrei-KS
+*/
+class CH18P645DRILLRunExmple : public RunCommandOfExample
+{
+public:
+  /*
+  * The main function launch to run a example.
+	* This function can use command line parameters
+	* via self variables itsArgC, itsArgV
+	*
+	* @see RunCommandOfExample
+	*/
+  virtual int excute() override;
+};
+
+#endif // !__CH18P645DRILL__
---
+++ Cpp/Stroustrup_principles_and_practice/Chapter_18/Example/CH18P645DRILL/src/CH18P645DRILLRunExmple.cpp
@@ -0,0 +1,54 @@
+/*
+	This file is generated from a layout that is in : {PROJECT_SOURCE_DIR}/include
+	date generate                                   : 04/11/2023 08:46:51
+	author                                          : Andrei-KS
+*/
+
+#include "CH18P645DRILLRunExmple.h"
+#include "std_lib_facilities.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH18P645DRILLRunExmple();
+
+namespace {
+  struct X {
+    int val;
+
+    void out(const string& s, int nv)
+    {
+      cerr << this << "->" << s << ": " << val << " (" << nv << ")\n";
+    }
+
+    X() { out("X()", 0); val = 0; }
+    X(int v) { val = v; out("X(int)", v); }
+    X(const X& x) { val = x.val; out("X(X&)", x.val); }
+    X& operator=(const X& a) { out("X::operator=()", a.val); val = a.val; return *this; }
+    ~X() { out("~X()", 0); }
+  };
+
+  X glob(2); //a global variable
+  X copy(X a) { return a; }
+  X copy2(X a) { X aa = a; return aa; }
+  X& ref_to(X& a) { return a; }
+  X* make(int i) { X a(i); return new X(a); }
+  struct XX { X a; X b; };
+}
+
+int CH18P645DRILLRunExmple::excute()
+{
+  X loc{ 4 }; //local variable
+  X loc2{ loc }; //copy construction
+  loc = X{ 5 }; //copy assignment
+  loc2 = copy(loc); //call by valueand return
+  loc2 = copy2(loc);
+  X loc3{ 6 };
+  X& r = ref_to(loc); //call by referenceand return
+  delete make(7);
+  delete make(8);
+  vector<X> v(4); //default values
+  XX loc4;
+  X* p = new X{ 9 }; //an X on the free store
+  delete p;
+  X* pp = new X[5]; //an array of Xs on the free store
+  delete[] pp;
+	return 0;
+}
