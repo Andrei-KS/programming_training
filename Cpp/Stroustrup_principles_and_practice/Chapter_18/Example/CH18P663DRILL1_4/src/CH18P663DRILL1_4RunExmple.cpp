51a1688bbb866fe2ffcdab10327e45b1690f28e0
Andrei-KS <74614116+Andrei-KS@users.noreply.github.com>
2023-04-20 09:17:06 +0200

    [Stroustrup_principles_and_practice] Added CH18P663DRILL1_4 example
    # CH18P663DRILL1_4          / Array drill:
    1. Define a global int array ga of ten ints initialized to 1, 2, 4, 8, 16, etc.
    2. Define a function f() taking an int array argument and an int argument indicating the number of elements in the array.
    3. In f():
     a. Define a local int array la of ten ints.
     b. Copy the values from ga into la.
     c. Print out the elements of la.
     d. Define a pointer p to int and initialize it with an array allocated on the free store with the same number of elements as the argument array.
     e. Copy the values from the argument array into the free-store array.
     f. Print out the elements of the free-store array.
     g. Deallocate the free-store array.
    4. In main():
     a. Call f() with ga as its argument.
     b. Define an array aa with ten elements, and initialize it with the first ten factorial values (1, 2*1, 3*2*1, 4*3*2*1, etc.).
     c. Call f() with aa as its argument.

---
+++ Cpp/Stroustrup_principles_and_practice/Chapter_18/Example/CH18P663DRILL1_4/src/CH18P663DRILL1_4RunExmple.cpp
@@ -0,0 +1,53 @@
+/*
+	This file is generated from a layout that is in : {PROJECT_SOURCE_DIR}/include
+	date generate                                   : 04/19/2023 08:50:12
+	author                                          : Andrei-KS
+*/
+
+#include "CH18P663DRILL1_4RunExmple.h"
+#include "std_lib_facilities.h"
+
+RunCommandOfExample* RunCommandOfExample::runCommandOfExample = new CH18P663DRILL1_4RunExmple();
+
+namespace {
+  int globalArraySize = 6;
+  int globalArray[] = { 1, 2, 4, 8, 16, 32 };
+
+  void f(int a[], int size)
+  {
+    const int LOCAL_ARRAY_SIZE = 10;
+    int localArray[LOCAL_ARRAY_SIZE];
+    for (int index = 0; index < LOCAL_ARRAY_SIZE && index < size; ++index)
+    {
+      localArray[index] = a[index];
+    }
+
+    cout << "\n";
+    for (int index = 0; index < LOCAL_ARRAY_SIZE; ++index)
+    {
+      cout << localArray[index] << " ";
+    }
+    cout << "\n";
+
+    int* p = new int[size];
+    for (int index = 0; index < size; ++index)
+    {
+      p[index] = a[index];
+    }
+    delete[] p;
+  }
+}
+
+int CH18P663DRILL1_4RunExmple::excute()
+{
+  f(globalArray, globalArraySize);
+  
+  const int AA_SIZE = 8;
+  int aa[AA_SIZE] = { 1 };
+  for (int index = 1; index < AA_SIZE; ++index)
+  {
+    aa[index] = aa[index - 1] * (aa[index-1] + 1);
+  }
+  f(aa, AA_SIZE);
+	return 0;
+}
